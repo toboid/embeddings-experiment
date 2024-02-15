914496d3f359fa42f7e22d782ab580f0edaa2000
Toby <toboid@users.noreply.github.com>
2024-02-15 07:55:17 +0000

    manually work out the cosine similarity

---
+++ cosine-similarity.js
@@ -0,0 +1,22 @@
+export const cosineSimilarity = (a, b) => {
+  if (a.length !== b.length) {
+    throw new Error('vectors must have the same length');
+  }
+
+  let dotProduct = 0;
+  let magnitudeA = 0;
+  let magnitudeB = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    dotProduct += a[i] * b[i];
+    magnitudeA += a[i] * a[i];
+    magnitudeB += b[i] * b[i];
+  }
+
+  magnitudeA = Math.sqrt(magnitudeA);
+  magnitudeB = Math.sqrt(magnitudeB);
+
+  const similarity = dotProduct / magnitudeA * magnitudeB);
+
+  return similarity;
+};
---
+++ find-similar-simple.js
@@ -0,0 +1,27 @@
+import { readFileSync } from 'node:fs';
+import { getEmbeddings } from './get-embedding.js';
+import { cosineSimilarity } from './cosine-similarity.js';
+
+const main = async () => {
+  console.log('Args', process.argv);
+  const question = process.argv[2];
+
+  const [questionEmbedding] = await getEmbeddings(question);
+
+  const reviewEmbeddings = JSON.parse(
+    readFileSync('./data/review-embeddings.json')
+  );
+
+  const matchingReviews = reviewEmbeddings
+    .map((e) => ({
+      ...e,
+      _distance: cosineSimilarity(e.vector, questionEmbedding),
+    }))
+    .sort((a, b) => b._distance - a._distance)
+    .slice(0, 5)
+    .map(({ vector, ...rest }) => ({ ...rest }));
+
+  console.log(matchingReviews);
+};
+
+main().then(console.log, console.error);
